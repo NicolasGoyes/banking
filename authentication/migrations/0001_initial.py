aa1430f54e772b457857c98f2f704c4bd21b2993
Nicolas Goyes <nygorecords@gmail.com>
2025-09-05 11:44:15 -0500

    initial corregido

--- authentication/migrations/0001_initial.py
+++ authentication/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='User',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('firname', models.CharField(max_length=20)),
+                ('firstname', models.CharField(max_length=20)),
             ],
         ),
     ]
