f482953c28a7c8f29075f9cfd52a9d61c4b58f4d
Nicolas Goyes <nygorecords@gmail.com>
2025-09-05 13:01:48 -0500

    Models

--- authentication/models.py
+++ authentication/models.py
@@ -3,5 +3,17 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+#    id_user = models.AutoField(primary_key=True)
+#    id_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="users")
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20, blank=True)
+
+class Department(models.Model):
+    name=models.CharField(max_length=20)
+    abrev=models.CharField(max_length=5)
+
+
+
+
+
+
